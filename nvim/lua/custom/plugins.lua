c11418450faaa1be3e9f296b99abe75316b9aa41
Anton Maminov <anton.linux@gmail.com>
2023-07-19 15:08:46 +0300

    nvim: add johnfrankmorgan/whitespace.nvim

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -58,6 +58,29 @@ local plugins = {
       require("better_escape").setup()
     end,
   },
+  {
+    'johnfrankmorgan/whitespace.nvim',
+    lazy = false,
+    config = function ()
+        require('whitespace-nvim').setup({
+            -- configuration options and their defaults
+
+            -- `highlight` configures which highlight is used to display
+            -- trailing whitespace
+            highlight = 'DiffDelete',
+
+            -- `ignored_filetypes` configures which filetypes to ignore when
+            -- displaying trailing whitespace
+            ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help' },
+
+            -- `ignore_terminal` configures whether to ignore terminal buffers
+            ignore_terminal = true,
+        })
+
+        -- remove trailing whitespace with a keybinding
+        vim.keymap.set('n', '<Leader>t', require('whitespace-nvim').trim, { desc = 'Remove trailing whitespaces'})
+    end
+  },
 
   -- To make a plugin not be loaded
   -- {
