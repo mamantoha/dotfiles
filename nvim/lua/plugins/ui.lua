450c2f751ecd1b0e41844be939a681bc4334cdb2
Anton Maminov <anton.linux@gmail.com>
2024-03-18 19:01:33 +0200

    nvim: add nvim-notify

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,7 +1,4 @@
 return {
-  -- ui components
-  { "MunifTanjim/nui.nvim", lazy = true },
-
   -- Active indent guide and indent text objects. When you're browsing
   -- code, this highlights the current level of indentation, and animates
   -- the highlighting.
@@ -73,6 +70,14 @@ return {
         long_message_to_split = true,
         inc_rename = true,
       },
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
     },
     -- stylua: ignore
     keys = {
