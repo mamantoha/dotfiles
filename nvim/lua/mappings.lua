3bba0b71a97f01e8e6cfa11122255bc414427871
Anton Maminov <anton.linux@gmail.com>
2024-06-05 13:55:50 +0300

    neovim: Toggle terminal

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,6 +3,7 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local unmap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
@@ -11,3 +12,10 @@ map("n", "<leader>fm", function()
 end, { desc = "File Format with conform" })
 
 map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
+
+unmap({ "n", "t" }, "<A-h>")
+
+-- Toggle terminal with Ctrl+/
+map({ "n", "t" }, "<C-_>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal new horizontal term" })
