3f63f5ee7a6f5e66078f235e71d475622a8cdeaf
Anton Maminov <anton.linux@gmail.com>
2024-08-01 16:30:25 +0300

    nvim: HILIGHT SETTINGS

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -10,6 +10,15 @@ M.ui = {
 M.base46 = {
   theme = "chadracula",
   theme_toggle = {"chadracula", "github_light"},
+  transparency = false,
+
+  -- HILIGHT SETTINGS
+  hl_add = {},
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+  integrations = {},
 }
 
 return M
